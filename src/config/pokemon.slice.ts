36e737fecb6524061c3dc6a144372bcdc9d72847
jessica-sobreira <j.jecristina@gmail.com>
2024-02-18 18:48:42 -0300

    redux

---
+++ src/config/pokemon.slice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const projetosSlice = createSlice({
+    name: "projetos",
+    initialState: null,
+    reducers: {},
+});
+
+export default projetosSlice.reducer;
---
+++ src/model/pokemon.model.ts
@@ -0,0 +1,17 @@
+
+export interface PokemonModel {
+    id: number;
+    nome: string;
+    tamanho: string;
+    habilidades: string;
+    imagem: string;
+    stats: {
+        hp: number;
+        ataque: number;
+        defesa: number;
+        ataque_especial: number;
+        defesa_especial: number;
+        velocidade: number;
+        total: number;    
+    }
+}
