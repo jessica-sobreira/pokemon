b8ce6971984bb206a2eb023c814858f18204323e
jessica-sobreira <j.jecristina@gmail.com>
2024-02-20 18:21:17 -0300

    redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Provider } from "react-redux";
-import { store } from "./config/store";
+import { persistedStore, store } from "./config/store";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
     return (
         <>
             <Provider store={store}>
+                <PersistGate persistor={persistedStore}>
                 <RouterProvider router={routes} />
+                </PersistGate>
             </Provider>
         </>
     );
--- src/config/rootReducer.ts
+++ src/config/rootReducer.ts
@@ -1,3 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 export const rootReducer = combineReducers({});
+
+export const persistedReducer = persistReducer(
+    {
+        key: "root",
+        storage: storage
+    },
+    rootReducer
+    )
