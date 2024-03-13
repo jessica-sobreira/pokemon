17f2fbe149fade8055ee54d3129a5d5c577462dd
jessica-sobreira <j.jecristina@gmail.com>
2024-03-13 17:47:01 -0300

    atualizando pokedex

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -6,62 +6,55 @@ import { useAppDispatch, useAppSelector } from '../config/hooks';
 import { addFavorite, removeFavorite } from '../config/modules/pokedexSlice'; 
 import { PokemonModel } from '../model/pokemon.model'; 
 
-
 interface PokedexCardProps {
     pokemon: PokemonModel;
 }
 
 export const PokedexCard: React.FC<PokedexCardProps> = ({ pokemon }) => {
     const dispatch = useAppDispatch();
-    const [openAlert, setOpenAlert] = useState<boolean>(false);
+    const [isAlertOpen, setIsAlertOpen] = useState<boolean>(false);
     const [alertMessage, setAlertMessage] = useState<string>('');
+
     const isPokemonInPokedex = useAppSelector((state) => state.pokedex.favorites.some(p => p.id === pokemon.id));
 
-    const handleAddFavorite = () => {
-        dispatch(addFavorite(pokemon));
-        const message = isPokemonInPokedex ? "Pokemon removido da Pokedex com sucesso!" : "Pokemon adicionado à Pokedex com sucesso!";
-        setAlertMessage(message);
-        setOpenAlert(true);
-    };
-
-    const handleRemoveFavorite = () => {
-        dispatch(removeFavorite(pokemon.id)); 
-        const message = "Pokemon removido da Pokedex com sucesso!";
-        setAlertMessage(message);
-        setOpenAlert(true);
+    const handleAddOrRemoveFavorite = () => {
+        if (isPokemonInPokedex) {
+            dispatch(removeFavorite(pokemon.id)); 
+            setAlertMessage("Pokemon removido da Pokedex com sucesso!");
+        } else {
+            dispatch(addFavorite(pokemon));
+            setAlertMessage("Pokemon adicionado à Pokedex com sucesso!");
+        }
+        setIsAlertOpen(true);
     };
 
     return (
         <>
-
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop: '0.2rem' }}>
-            <Card sx={{ paddingTop: '10px', backgroundColor: '#f5f5f5', height: 'auto' }}>
-                <Button onClick={isPokemonInPokedex ? handleRemoveFavorite : handleAddFavorite} sx={{ color: isPokemonInPokedex ? 'red' : 'black', alignSelf: 'end', display: 'flex', paddingInlineStart: '15px' }}>
-                    {isPokemonInPokedex ? <DeleteIcon /> : <StarIcon />} 
-                </Button>
-
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card sx={{ maxWidth: 300, backgroundColor: '#f5f5f5', margin: 'auto' }}>
                 <CardMedia
-                    sx={{ borderRadius: '20px' }}
                     component="img"
                     alt={pokemon.name}
                     height='240'
                     width='240'
                     image={pokemon.sprites.front_default}
+                    sx={{ borderRadius: '20px 20px 0 0' }}
                 />
-
                 <CardContent>
-                    <Typography variant="body1" color="black" style={{ textAlign: 'center' }}><strong>{pokemon.name.toUpperCase()}</strong></Typography>
-                    
-
+                    <Typography variant="body1" color="black" align="center"><strong>{pokemon.name.toUpperCase()}</strong></Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+                        <Button onClick={handleAddOrRemoveFavorite} variant="contained" color={isPokemonInPokedex ? 'error' : 'primary'}>
+                            {isPokemonInPokedex ? <DeleteIcon /> : <StarIcon />} 
+                            {isPokemonInPokedex ? 'Remover da Pokedex' : 'Adicionar à Pokedex'}
+                        </Button>
+                    </Box>
                 </CardContent>
             </Card>
 
-            <Snackbar className='styleAlert' open={openAlert} autoHideDuration={1900} onClose={() => setOpenAlert(false)}>
-                <Alert variant='filled' onClose={() => setOpenAlert(false)} severity="success">{alertMessage}</Alert>
+            <Snackbar open={isAlertOpen} autoHideDuration={1900} onClose={() => setIsAlertOpen(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert onClose={() => setIsAlertOpen(false)} severity="success" sx={{ width: '100%' }}>{alertMessage}</Alert>
             </Snackbar>
         </Box>
-
-        
         </>
     );
 };
